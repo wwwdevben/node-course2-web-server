fd537cfe1344a657c3340f020b3398bdb297a9c9
wwwdevben <deliverybyben@yahoo.com>
2018-07-07 23:20:22 -0400

    intial commit

---
+++ server.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log = `${now}: ${req.method}: ${req.url}`;
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if (err) {
+            console.log(err);
+        }
+    });
+    next();
+});
+
+// app.use((req,res,next) => {
+//    res.render('matin');
+// });
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+});
+
+app.get('/', (req,res) => {
+    res.render('home', {
+        pageTitle: 'Home',
+        welcomeMessage: 'Hello and welcome to some website'
+    });
+});
+
+app.get('/about', (req,res)=> {
+   res.render('about', {
+       pageTitle: 'About Page'
+   });
+});
+
+app.get('/bad', (req,res) => {
+    res.send({
+        errorMessage: 'Unable to fufill this request'
+    });
+});
+
+app.use(express.static(__dirname + '/public'));
+
+
+app.listen(3000, () => {
+    console.log('Server up on p3000');
+});
