5c9f79a6fc26f2c1722a86e9af844f7d9693b3f6
wwwdevben <deliverybyben@yahoo.com>
2018-07-07 23:48:54 -0400

    Ready for deploy

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
+const port = process.env.PORT || 3000;
 
 var app = express();
 
@@ -53,6 +54,6 @@ app.get('/bad', (req,res) => {
 app.use(express.static(__dirname + '/public'));
 
 
-app.listen(3000, () => {
-    console.log('Server up on p3000');
+app.listen(port, () => {
+    console.log('Server up on port ' + port);
 });
